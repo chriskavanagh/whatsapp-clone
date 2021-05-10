f46cda1f78c82b1b64ea35cfa5b495cc5f4c36c7
Chris Kavanagh <ckava3@gmail.com>
2021-05-09 23:42:29 -0400

    First

---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+import { v4 as uuidV4 } from "uuid";
+import { useRef } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+
+export default function Login() {
+  return (
+    <Container
+      className="align-items-center d-flex"
+      style={{ height: "100vh" }}
+    >
+      <h4>Login</h4>
+    </Container>
+  );
+}
